2ab05ecc8216313146f5262b85cddaadb7785f5f
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-01-29 08:28:29 -0300

    Criado a conexão com o BD e o método insert_data

---
+++ src/connection.py
@@ -0,0 +1,22 @@
+import pymysql
+
+# abrir conexáo
+conn = pymysql.connect(
+    host="localhost",
+    port=3306,
+    database="dt-neoway",
+    user="root",
+    password="",
+    autocommit=True
+)
+
+
+def insert_data(cpf, name, score):
+    # criar cursor
+    cursor = conn.cursor()
+    # inserir no banco
+    sql = "INSERT INTO students(cpf, name, score) values (%s, %s, %s)"
+    values = (cpf, name, score)
+    cursor.execute(sql, values)
+    # fechar conexão
+    conn.close()
