499cf9fe7979ee706632b82975bfa73747acbb40
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-01-30 04:05:31 -0300

    Criado no arquivo main.py a função auxiliar para coletar os links dos cpfs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import requests
+from parsel import Selector
 
 
 # função auxiliar para requisição html
@@ -11,6 +12,14 @@ def fetch_content(html_content):
     return res.text
 
 
+# função auxiliar para coletar os links dos cpfs
+def scrape_cpf_path(html_content):
+    selector = Selector(html_content)
+    result = selector.css("li a ::attr(href)").getall()
+    return result
+
+
 if __name__ == "__main__":
-    base_url = "https://sample-university-site.herokuapp.com/"
+    base_url = "https://sample-university-site.herokuapp.com"
     quotes_html = fetch_content(base_url)
+    cpf_paths = scrape_cpf_path(quotes_html)
