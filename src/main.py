d807d876db04458ada3f0f996a8e50b2b59895be
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-01-31 07:07:09 -0300

    Fazendo limpeza de dados nos nomes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from parsel import Selector
 from connection import insert_data, close_connection
 
@@ -46,12 +47,17 @@ if __name__ == "__main__":
     base_url = "https://sample-university-site.herokuapp.com"
     quotes_html = fetch_content(base_url)
     cpf_paths = scrape_cpf_path(quotes_html)
+    regex = r"""
+        |(Dr.\s)|(Mr.\s)|(Mrs.\s)|(Ms.\s)|(Jr.\s)|(Sr.\s)|(Miss)|
+        |(I\s)|(II)|(III)|(IV)|(\sV\s)|(MD)|(DDS)|(PhD)|(DVM)|
+    """
 
 
 # coletando os links dos cpfs de todas as páginas
     print("Loading...")
     while True:
         next_page = scrape_next_page_path(quotes_html)
+        # if not next_page:
         if next_page == "/approvals/6":
             break
         quotes_html = fetch_content(base_url + next_page)
@@ -62,8 +68,9 @@ if __name__ == "__main__":
     for path in cpf_paths:
         student_cpf = path.split("/")[-1].replace("-", "").replace(".", "")
         students_html = fetch_content(base_url + path)
-        student_name = scrape_student_name(students_html).strip().upper()
+        student_name = scrape_student_name(students_html)
+        clear_name = re.sub(regex, "", student_name).strip()
         student_score = scrape_student_score(students_html).strip()
-        insert_data(student_cpf, student_name, float(student_score))
+        insert_data(student_cpf, clear_name.upper(), float(student_score))
     close_connection()
     print("Done!")
